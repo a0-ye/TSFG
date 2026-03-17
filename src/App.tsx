b47925d374ae9210432aebb6745c5a50e7f77464
Adrian Y. <acye@ucsd.edu>
2026-03-17 15:13:21 -0500

    separated story viewer into separate component. Added Node based Story Editor. No export tool yet

--- src/App.tsx
+++ src/App.tsx
@@ -1,209 +1,24 @@
-import { useEffect, useRef, useState } from 'react'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 import './App.css'
-import PassageBox from './components/PassageBox/PassageBox'
-import Journal from './components/Journal/Journal'
-import { renderAsync } from 'docx-preview'
-import JSZip from 'jszip'
+import StoryViewer from './components/StoryViewer/StoryViewer'
+import StoryEditor from './components/StoryEditor/StoryEditor'
 
-import { ERROR_NODE, type AllNodes, type DataNode, type JournalNode } from './components/NodeTypes'
-import type { FlagValue } from './components/utils'
+function App(){
+  return <>
+    <BrowserRouter>
+      <nav>
+        <Link to="/"> StoryViewer </Link>
+        <Link to="/Editor"> StoryEditor </Link>
+      </nav>
 
-function App() {
-  /**
-   * TODO:
-   * Passage Event System ? what does this even mean? Sound triggers. visual triggers. Timer triggers? Events should be on Passage Load.
-   * replace Map usage with Record<T,T>()
-   */
-
-
-  const [displayedPassages, setDisplayedPassages] = useState<string[]>([])  // An array of passage IDs to display
-  const addPassage = (passageID: string) => {
-    setDisplayedPassages((prevPassages) => {
-      return [...prevPassages, passageID]
-    })
-  }
-  const [fileLoaded, setFileLoaded] = useState(false)
-  const [nodeMap, setNodeMap] = useState(new Map<string, AllNodes>()) // A map containing all of the passages as id:node key:val
-  const [journalMap, setJournalMap] = useState(new Map<string, JournalNode>()) // map of all journal entries as id:node
-
-  const [flags, setFlags] = useState<Record<string, FlagValue>>({})
-  /**
- * Updates Journal Flags with the all flag:value in newFlags.
- * should not contain any operators since this is a flat value update
- * @param newFlags JS object of string: string | number | boolean. 
- */
-  function updateFlags(newFlags: Record<string, FlagValue>) {
-    setFlags((prev) => {
-      const newObj = { ...prev, ...newFlags }
-      console.log("Updated Flags", newObj);
-      return newObj
-    });
-
-  }
-  const [displayedJournalEntries, setDisplayedJournalEntries] = useState<JournalNode[]>([])    // list of nodes. should be based on groupID used to keep track of chronological entries
-
-  /**
-   * Runs when the game is restarted. NOT WIPED, but restarted.
-   * 
-   * Should reset all flags
-   * Should remove all non-persistant displayed entries
-   */
-  function restartRun() {
-    setFlags({})
-    // filter nodes whose persist flag is true
-    setDisplayedJournalEntries((prev) => prev.filter((node) => node.persist === true))
-  }
-
-  /**
-   * RESETS EVERYTHING. Wipes the slate clean
-   */
-  function resetAll() {
-    setFlags({})
-    setDisplayedJournalEntries([])
-    setFileLoaded(false)
-  }
-
-  /**
-   * 
-   * Extracts the story data from the zip file, and loads it into the global passageMap
-   */
-  const loadStory = async (event: React.ChangeEvent<HTMLInputElement>) => {
-    const ghostDiv: HTMLElement = document.createElement('div');  // temp, used to extract the story content from docx
-    const file = event.target.files?.[0];
-    if (!file) { return; }
-    try {
-      // Extract json and docx from zip
-      const zip = await JSZip.loadAsync(file);
-      const jsonFile = zip.filter((path, _) => path.endsWith(".json"))[0];
-      const jsonData = jsonFile ? await jsonFile.async("string") : "{}";
-      const nodeData: Record<string, any> = JSON.parse(jsonData);
-
-
-      const docxFile = zip.filter((path, _) => path.endsWith(".docx"))[0];
-      if (!docxFile) {
-        console.error("ERRO ON FILE LOAD: No .docx file found in the package");
-      }
-      const docxBlob = docxFile ? await docxFile.async("blob") : null;
-
-      // load docx into ghostDiv
-      await renderAsync(docxBlob, ghostDiv);
-
-      // read from the ghostDiv
-      const tables = ghostDiv.querySelectorAll('article table');
-      const nodeMap = new Map()
-      const journalMap = new Map<string, JournalNode>()
-
-      // Everything is based on the Tables get created first, so if there is JSON data for a non-existent table in the DOCX, no DataNode will ever be created
-      const tableNodes = Array.from(tables).map((table, index) => {
-        const htmlTable = table as HTMLTableElement;
-        const rows = Array.from(htmlTable.rows);
-        const rawID = rows[0].cells[0].textContent?.trim() || `noIDError|Table-${index}`;
-
-
-        // slice from index 1 to capture everything after the ID row
-        const contentData = rows.slice(1).map(row =>
-          Array.from(row.cells).map(cell => cell.innerHTML)[0]
-        );
-        const specificNodeData = nodeData.find((node: any) => node.id == rawID); // TODO: fix typing here
-        if (specificNodeData?.type == '2') {
-          if (rows[2].cells[0].textContent.trim() == '') { contentData[1] = contentData[0] } // if the text content is empty, copy from the other row
-          // other stuff maybe          
-        }
-
-        const output: DataNode = {
-          ...ERROR_NODE,  // if we cant find anything its just an error
-          id: rawID,
-          data: contentData,
-          rowCount: rows.length,
-          ...specificNodeData // overwrite the default errors for type & next
-        };
-        nodeMap.set(rawID, output)
-        if (specificNodeData.type == '4') {  // add as only journal node
-          journalMap.set(rawID, output as JournalNode)
-          // console.log(journalMap.get(rawID));
-        }
-        return output;
-      });
-      setNodeMap(nodeMap)
-      setJournalMap(journalMap)
-      addPassage(tableNodes[0].id)  // DEBUG AUTO ADD THE FIRST ONE
-    } catch (error) {
-      console.error("Failed to load story nodes from DOCX ", error);
-
-    }
-
-    setFileLoaded(true)
-  }
-
-
-  const bottomRef = useRef<HTMLDivElement>(null);
-  const jumpToBottom = () => {
-    if (bottomRef.current) {
-      bottomRef.current.scrollIntoView({ behavior: 'smooth' });
-    }
-  }
-  useEffect(jumpToBottom, [displayedPassages]);
-
-  return (
-    <>
-      <Journal
-        flags={flags}
-        setFlags={setFlags}
-        displayedJournalEntries={displayedJournalEntries}
-        setDisplayedJournalEntries={setDisplayedJournalEntries}
-        journalMap={journalMap}
-      ></Journal>
-
-      <div id='triColSplit' style={{ display: 'grid', gridTemplateColumns: ' 15vw 70vw 15vw ' }}>
-        <div id='leftContent' className='sideCol' style={{}}></div>
-        <div id='centerContent'>
-          <div style={{ position: 'fixed', top: 0, right: 0 }}>
-            <button onClick={() => {
-              resetAll()
-            }}>Reset ALL (wipes progress)</button>
-            <button onClick={() => {
-              restartRun()
-              // DOES NOT RESET THE JOURNAL. 
-            }}>Restart Story (keep journal progress) </button>
-            <button onClick={jumpToBottom}> Jump To Bottom </button>
-          </div>
-          {!fileLoaded && <div>
-            <p>
-              Upload your .zip file to begin. It should contain both the .json and the .docx
-            </p>
-            <input type='file' accept='.zip' onChange={loadStory}></input>
-          </div>}
-
-          {
-            // fileLoaded && <DOCXNodeViewer nodes={storyNodes} />
-          }
-
-          <div>
-            {displayedPassages.map((passageID, index) => (
-              <>
-                {
-                  // should pass it:
-                  // The ID, the Json data, the passage map. The intent is that inside, it looks up passage content on its own.d
-                }
-                <PassageBox
-                  passageID={passageID}
-                  passageMap={nodeMap}
-                  addPassage={addPassage}
-                  index={index}
-                  flags={flags}
-                  updateFlags={updateFlags}
-                ></PassageBox>
-              </>
-            ))}
-
-
-            <div id='bottomRef' ref={bottomRef} style={{ height: '20vh' }} />
-          </div>
-        </div>
-        <div id='rightContent' className='sideCol' style={{}}></div>
-      </div>
-    </>
-  )
+      <Routes>
+        <Route path='/' element={<StoryViewer/>}/>
+        <Route path='/Editor' element={<StoryEditor/>}/>
+      </Routes>
+    
+    </BrowserRouter>
+  
+  </>
 }
 
 export default App
---
+++ src/components/StoryEditor/StoryEditor.tsx
@@ -0,0 +1,401 @@
+
+import React, { useCallback, useRef, useState } from 'react';
+import {
+    Background,
+    ReactFlow,
+    useNodesState,
+    useEdgesState,
+    reconnectEdge,
+    addEdge,
+    type Connection,
+    type NodeMouseHandler,
+    Handle,
+    Position,
+    MarkerType,
+    BaseEdge,
+    type EdgeProps,
+    EdgeLabelRenderer,
+    getSmoothStepPath,
+    useReactFlow,
+    type NodeProps,
+} from '@xyflow/react';
+
+import '@xyflow/react/dist/style.css';
+import { renderAsync } from 'docx-preview';
+import { ERROR_NODE, type DataNode, type JournalNode } from '../NodeTypes';
+
+
+/**
+ * Node that can have vars set. Stored in the node's data prop as vars: data.vars
+ */
+function VarSetNode({ id, data }: NodeProps) {
+    const { setNodes } = useReactFlow();
+
+    const currentVars = (data?.vars as Array<{ key: string, value: string }>) || [];
+    const updateGlobal = (newVars: Array<{ key: string, value: string }>) => {
+        setNodes((nds) => nds.map((node) => {
+            if (node.id === id) {
+                return { ...node, data: { ...node.data, vars: newVars } };
+            }
+            return node;
+        }));
+    };
+
+    const addVar = () => updateGlobal([...currentVars, { key: '', value: '' }]);
+
+    const removeVar = (index: number) => {
+        updateGlobal(currentVars.filter((_, i) => i !== index));
+    };
+
+    const updateVar = (index: number, field: string, value: string) => {
+        const next = [...currentVars];
+        next[index] = { ...next[index], [field]: value };
+        updateGlobal(next);
+    };
+
+    return (
+        <div id='VarSetNodeContainer' style={{ backgroundColor: '#fff', borderRadius: '5px', padding: '5px', fontSize: 'x-small' }}>
+            <label style={{ color: 'black', }}>{id}</label>
+            <div style={{ display: 'flex', flexDirection: 'column', gap: '4px', color: 'black' }}>
+                {/* <label>Variables Set</label> */}
+                {currentVars.map((v, i) => (
+                    <div key={i} style={{ display: 'flex', gap: '1px' }}>
+                        <input
+                            className="nodrag"
+                            placeholder="Var"
+                            value={v.key}
+                            onChange={(e) => updateVar(i, 'key', e.target.value)}
+                            style={{ width: '60px', fontSize: 'xx-small' }}
+                        />
+                        <input
+                            className="nodrag"
+                            placeholder="Value"
+                            value={v.value}
+                            onChange={(e) => updateVar(i, 'value', e.target.value)}
+                            style={{ width: '60px', fontSize: 'xx-small' }}
+                        />
+                        <button onClick={() => removeVar(i)}>X</button>
+                    </div>
+                ))}
+
+                <button onClick={addVar} style={{ fontSize: 'xx-small' }}>+ Add Var</button>
+            </div>
+
+            <Handle type="target" position={Position.Top} />
+            <Handle type="source" position={Position.Bottom} />
+        </div>
+    );
+}
+const edgeTypes = {
+    'custom-edge': CustomEdge,
+};
+function CustomEdge({ id, sourceX, sourceY, targetX, targetY, data }: EdgeProps) {
+    const [edgePath, labelX, labelY] = getSmoothStepPath({
+        sourceX,
+        sourceY,
+        targetX,
+        targetY,
+    });
+    const { setEdges } = useReactFlow();
+
+    const currentVars = (data?.vars as Array<{ key: string, value: string }>) || [];
+    const updateGlobal = (newVars: Array<{ key: string, value: string }>) => {
+        setEdges((edges) => edges.map((edge) => {
+            if (edge.id === id) {
+                return { ...edge, data: { ...edge.data, vars: newVars } };
+            }
+            return edge;
+        }));
+    };
+
+    const addVar = () => updateGlobal([...currentVars, { key: '', value: '' }]);
+    const removeVar = (index: number) => {
+        updateGlobal(currentVars.filter((_, i) => i !== index));
+    };
+
+    const updateVar = (index: number, field: string, value: string) => {
+        const next = [...currentVars];
+        next[index] = { ...next[index], [field]: value };
+        updateGlobal(next);
+    };
+    return (
+        <>
+            <BaseEdge id={id} path={edgePath} />
+            <EdgeLabelRenderer>
+                <div id='EdgeDependenciesContainer' style={{
+                    textAlign: 'left',
+                    transform: `translate(-50%, -50%) translate(${labelX}px,${labelY}px)`,
+                    pointerEvents: 'all',
+                    width: 'min-content',
+                    fontSize: 'x-small'
+                }}>
+                    <div style={{ display: 'flex', flexDirection: 'column', gap: '4px' }}>
+                        <label>Dependencies</label>
+                        {currentVars.map((v, i) => (
+                            <div key={i} style={{ display: 'flex', gap: '2px' }}>
+                                <input
+                                    className="nodrag" // Prevents dragging node while typing
+                                    placeholder="Var"
+                                    value={v.key}
+                                    onChange={(e) => updateVar(i, 'key', e.target.value)}
+                                    style={{ width: '60px', fontSize: 'inherit' }}
+                                />
+                                <input
+                                    className="nodrag"
+                                    placeholder="Value"
+                                    value={v.value}
+                                    onChange={(e) => updateVar(i, 'value', e.target.value)}
+                                    style={{ width: '60px', fontSize: 'inherit' }}
+                                />
+                                <button onClick={() => removeVar(i)} style={{ padding: '0 4px', cursor: 'pointer' }}>X</button>
+                            </div>
+                        ))}
+                        <button onClick={addVar}>add dependency</button>
+                    </div>
+
+                </div>
+            </EdgeLabelRenderer>
+        </>
+    );
+}
+
+const nodeTypes = {
+    VarSetNode: VarSetNode
+}
+
+const initialNodes: {
+    id: string;
+    data: {
+        label: string;
+    };
+    position: {
+        x: number;
+        y: number;
+    };
+}[] = [
+        // {
+        //     id: '1',
+        //     data: { label: 'Node A' },
+        //     position: { x: 250, y: 0 },
+        // },
+        // {
+        //     id: '2',
+        //     data: { label: 'Node B' },
+        //     position: { x: 100, y: 200 },
+        // },
+        // {
+        //     id: '3',
+        //     data: { label: 'Node C' },
+        //     position: { x: 350, y: 200 },
+        // },
+    ];
+
+const initialEdges: {
+    id: string;
+    source: string;
+    target: string;
+    label: string;
+}[] = [];
+
+/**
+ * id: 'edge-button',
+    source: 'button-1',
+    target: 'button-2',
+    type: 'buttonedge',
+  },
+ */
+/**
+ *  Using React Flow, we can make a node editor in browser to make and edit stories!
+ * 
+ * NEED: 
+ * - local storage so progress isnt reset upon refreshing or crashes
+ * - export as json
+ * - maybe package DOCX & JSON together finally? node text previewer
+ *          - specifically, load docx into the map. Have a list of IDs on the side. on click, preview the text inside, 
+ *              have the option to create a node for it or if it exists dont do that
+ * 
+ * 
+ * PLAN: do Varset in the node itself, do dependencies in the edges. Make edges have one way arrows.
+ * 
+ * create custom node and edge component
+ * 
+ * When exporting, we can just package everything nicely into a JSON. grab nodes, add next[target, target] from any edge with source = node
+ */
+
+export default function StoryEditor() {
+    const edgeReconnectSuccessful = useRef(true);
+    const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+    const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+    const onConnect = useCallback((params: any) => setEdges((els) => addEdge({ ...params, markerEnd: { type: MarkerType.ArrowClosed }, type: 'custom-edge' }, els)), [],);
+    const onReconnectStart = useCallback(() => { edgeReconnectSuccessful.current = false; }, []);
+    const onReconnect = useCallback((oldEdge: any, newConnection: Connection) => {
+        edgeReconnectSuccessful.current = true;
+        setEdges((els) => reconnectEdge(oldEdge, newConnection, els));
+    }, []);
+    const onReconnectEnd = useCallback((_: any, edge: { id: string; }) => {
+        if (!edgeReconnectSuccessful.current) {
+            setEdges((eds) => eds.filter((e) => e.id !== edge.id));
+        }
+        edgeReconnectSuccessful.current = true;
+    }, []);
+    const onNodeClick = useCallback<NodeMouseHandler>(
+        (_, node) => {
+            setSelectedDocxNodeId(node.id)
+        },
+        [],
+    );
+
+
+    const [fileLoaded, setFileLoaded] = useState(false)
+    const [docxNodes, setDocxNodes] = useState<DataNode[]>([]);
+    const [selectedDocxNodeId, setSelectedDocxNodeId] = useState<string | null>(null);
+    const activeNode = docxNodes.find(n => n.id === selectedDocxNodeId);
+    const loadStory = async (event: React.ChangeEvent<HTMLInputElement>) => {
+        const ghostDiv: HTMLElement = document.createElement('div');  // temp, used to extract the story content from docx
+        const file = event.target.files?.[0];
+        if (!file) { return; }
+        try {
+            const docxFile = file.arrayBuffer()
+            if (!docxFile) {
+                console.error("ERRO ON FILE LOAD: No .docx file found in the package");
+            }
+            // load docx into ghostDiv
+            await renderAsync(file, ghostDiv);
+
+            // read from the ghostDiv
+            const tables = ghostDiv.querySelectorAll('article table');
+            const nodeMap = new Map()
+            const journalMap = new Map<string, JournalNode>()
+
+            // Everything is based on the Tables get created first, so if there is JSON data for a non-existent table in the DOCX, no DataNode will ever be created
+            const tableNodes = Array.from(tables).map((table, index) => {
+                const htmlTable = table as HTMLTableElement;
+                const rows = Array.from(htmlTable.rows);
+                const rawID = rows[0].cells[0].textContent?.trim() || `noIDError|Table-${index}`;
+
+
+                // slice from index 1 to capture everything after the ID row
+                const contentData = rows.slice(1).map(row =>
+                    Array.from(row.cells).map(cell => cell.innerHTML)[0]
+                );
+
+                const output: DataNode = {
+                    ...ERROR_NODE,  // if we cant find anything its just an error
+                    id: rawID,
+                    data: contentData,
+                    rowCount: rows.length,
+                };
+                nodeMap.set(rawID, output)
+                return output;
+            });
+            setDocxNodes(tableNodes);
+            if (tableNodes.length > 0) {
+                setSelectedDocxNodeId(tableNodes[0].id);
+                setFileLoaded(true);
+            }
+        } catch (error) {
+            console.error("Failed to load story nodes from DOCX ", error);
+
+        }
+
+    }
+
+
+    return <>
+        <div id='StoryEditorBase' style={{
+            width: '100vw', height: '100vh',
+            display: 'grid', gridTemplateColumns: '300px 1fr 100px',
+
+        }}>
+            <div id='left panel' style={{ display: 'grid', gridTemplateRows: '50vh 50vh', }}>
+                <div style={{ overflow: 'scroll' }}>
+                    {!fileLoaded ? <div>
+                        <p>
+                            Upload your story docx here!
+                        </p>
+                        <input type='file' accept='.docx' onChange={loadStory}></input>
+                    </div> : (
+                        <div style={{ display: 'flex', flexDirection: 'column', gap: '5px' }}>
+                            <h3>Nodes ({docxNodes.length})</h3>
+                            {docxNodes.map(node => (
+                                <button
+                                    key={node.id}
+                                    onClick={() => setSelectedDocxNodeId(node.id)}
+                                    style={{
+                                        textAlign: 'left',
+                                        padding: '8px',
+                                        cursor: 'pointer',
+                                        backgroundColor: selectedDocxNodeId === node.id ? '#444' : 'transparent',
+                                        border: '1px solid #555',
+                                        color: 'white',
+                                        borderRadius: '4px'
+                                    }}
+                                >
+                                    {node.id}
+                                </button>
+                            ))}
+                        </div>
+                    )}
+                </div>
+                <div>
+                    Preview:
+                    {fileLoaded &&
+                        <div style={{ textAlign: 'left', padding: '1em' }} dangerouslySetInnerHTML={{ __html: activeNode?.data || 'ERROR no data found' }} />
+
+                    }
+                    {/* <button onClick={() => {
+                        setNodes((prev) => {
+                            const newNode = {
+                                id: activeNode?.id || 'ERROR',
+                                data: {
+                                    label: activeNode?.id || 'ERROR'
+                                },
+                                position: { x: 250, y: 0 },
+                            }
+                            return [...prev, newNode]
+                        })
+                    }}> Click to add as Node </button> */}
+                    <button onClick={() => {
+                        setNodes((prev) => {
+                            const newNode = {
+                                id: activeNode?.id || 'ERROR',
+                                data: {
+                                    label: activeNode?.id || 'ERROR'
+                                },
+                                position: { x: 250, y: 0 },
+                                type: 'VarSetNode'
+                            }
+                            return [...prev, newNode]
+                        })
+                    }}> Click to add as VarSetNode </button>
+                </div>
+            </div>
+            <ReactFlow style={{ color: '#242424ff', backgroundColor: '#616161ff' }}
+                nodes={nodes}
+                edges={edges}
+                nodeTypes={nodeTypes}
+                edgeTypes={edgeTypes}
+                onNodesChange={onNodesChange}
+                onEdgesChange={onEdgesChange}
+                onNodeClick={(onNodeClick)}
+                snapToGrid
+                onReconnect={onReconnect}
+                onReconnectStart={onReconnectStart}
+                onReconnectEnd={onReconnectEnd}
+                onConnect={onConnect}
+                fitView
+                attributionPosition="top-right"
+            >
+                <Background />
+            </ReactFlow>
+            <div> right side
+                <button onClick={() => {
+                    console.log(nodes, edges);
+                }}> click to export </button>
+
+            </div>
+        </div>
+
+
+    </>
+}
